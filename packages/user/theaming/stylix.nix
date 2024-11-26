406a3c4fb14bb22a5194a8969a2c82f659be3f30
Domz4 <domrza99@gmail.com>
2024-11-26 12:13:18 +0100

    stylix changes

--- packages/user/theaming/stylix.nix
+++ packages/user/theaming/stylix.nix
@@ -9,43 +9,44 @@
 
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     # *** CUSTOM THEME ***
+    # 
     base16Scheme = {
-      base00 = "1A1B26";
-      base01 = "16161E";
-      base02 = "2F3549";
-      base03 = "444B6A";
-      base04 = "787C99";
-      base05 = "A9B1D6";
-      base06 = "CBCCD1";
-      base07 = "D5D6DB";
-      base08 = "C0CAF5";
-      base09 = "A9B1D6";
-      base0A = "0DB9D7";
-      base0B = "9ECE6A";
-      base0C = "B4F9F8";
-      base0D = "2AC3DE";
-      base0E = "BB9AF7";
-      base0F = "F7768E";
+      base00 = "091212";
+      base01 = "242B2A";
+      base02 = "3C403F";
+      base03 = "4E5150";
+      base04 = "62d6e8";
+      base05 = "DEFFFC";
+      base06 = "ffffff";
+      base07 = "73949C";
+      base08 = "309Cb7";
+      base09 = "95BBC4";
+      base0A = "3edb84";
+      base0B = "ddb350";
+      base0C = "9bf9fd";
+      base0E = "aF8DfA";
+      base0D = "FF0055";
+      base0F = "FFFFFF";
     };
 
     # THEME FROM WALLPAPER
     image = ./wallpaper.jpg;
 
-    # FONTS
+    # # FONTS
     fonts = {
       monospace = {
-        package = pkgs.monaspace.override { fonts = [ "Monaspace Krypton" ]; };
-        name = "Monaspace Krypton";
+        package = pkgs.nerdfonts.override { fonts = [ "Monaspice" ]; };
+        name = "Monaspice Krypton";
       };
 
       sansSerif = {
-        package = pkgs.monaspace.override { fonts = [ "Monaspace Krypton" ]; };
-        name = "Monaspace Krypton";
+        package = pkgs.nerdfonts.override { fonts = [ "Monaspice" ]; };
+        name = "Monaspice Krypton";
       };
 
       serif = {
-        package = pkgs.monaspace.override { fonts = [ "Monaspace Krypton" ]; };
-        name = "Monaspace Krypton";
+        package = pkgs.nerdfonts.override { fonts = [ "Monaspice" ]; };
+        name = "Monaspice Krypton";
       };
 
       sizes = {
