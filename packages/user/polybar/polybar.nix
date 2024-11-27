a432fa1f6439e8366bcbc0249302501e4e57c1e2
Domz4 <domrza99@gmail.com>
2024-11-27 17:59:29 +0100

    configure in progress ...

--- packages/user/polybar/polybar.nix
+++ packages/user/polybar/polybar.nix
@@ -17,77 +17,80 @@ in
 
   services.polybar = {
     enable = true;
-    config = {
-      "global/wm" = {
-        margin-bottom = "0";
-        margin-top = "0";
-        include-file ="~/.config/polybar/shades/bars.ini";
-        include-file ="~/.config/polybar/shades/colors.ini";
-        include-file ="~/.config/polybar/shades/modules.ini";
-        include-file ="~/.config/polybar/shades/user_modules.ini";
-      };
-      "bar/main" = {
-      monitor-strict = "false";
-      override-redirect = "false";
-      bottom = "false";
-      fixed-center = "true";
-      width = "98%";
-      height = "36";
-      offset-x = "1%";
-      offset-y = "2%";
-      background = "${color.background}";
-      foreground = "${color.foreground}";
-      radius-top = "0.0";
-      radius-bottom = "0.0";
-      underline-size = "2";
-      underline-color = "${color.foreground}";
-      border-size = "0";
-      border-color = "${color.background}";
-      padding = "0";
-      module-margin-left = "0";
-      module-margin-right = "0";
-      font-0 = "Iosevka Nerd Font:pixelsize=14;4";
-      font-1 = "Iosevka Nerd Font:pixelsize=14;4";
-      modules-left = "launcher workspaces google github reddit gmail twitter";
-      modules-center = "";
-      modules-right = "mpd updates alsa battery network date sysmenu";
-      separator ="";
-      dim-value = "1.0";
-      wm-name = "";
-      locale = "";
-      tray-position = "none";
-      tray-detached = "false";
-      tray-maxsize = "16";
-      tray-background = "${color.background}";
-      tray-offset-x = "0";
-      tray-offset-y = "0";
-      tray-padding = "0";
-      tray-scale = "1.0";
-      enable-ipc = "true";
-      click-left ="" ;
-      click-middle ="" ;
-      click-right ="";
-      scroll-up ="";
-      scroll-down ="";
-      double-click-left ="";
-      double-click-middle ="";
-      double-click-right ="";
-      cursor-click = "";
-      cursor-scroll = "";
-      };
-      "settings" = {;
-      throttle-output = "5";
-      throttle-output-for = "10";
-      screenchange-reload = "false";
-      compositing-background = "source";
-      compositing-foreground = "over";
-      compositing-overline = "over";
-      compositing-underline = "over";
-      compositing-border = "over";
-      pseudo-transparency = "false";
-      ]
-      };
-    };
+    config = ./config.ini;
+    # config = {
+    #   "global/wm" = {
+    #     margin-bottom = "0";
+    #     margin-top = "0";
+    #     include-files = [
+    #       "${config.home.homeDirectory}/.config/polybar/shades/bars.ini"
+    #       "${config.home.homeDirectory}/.config/polybar/shades/colors.ini"
+    #       "${config.home.homeDirectory}/.config/polybar/shades/modules.ini"
+    #       "${config.home.homeDirectory}/.config/polybar/shades/user_modules.ini"
+    #     ];
+    #   };
+    #   "bar/main" = {
+    #     monitor-strict = "false";
+    #     override-redirect = "false";
+    #     bottom = "false";
+    #     fixed-center = "true";
+    #     width = "98%";
+    #     height = "36";
+    #     offset-x = "1%";
+    #     offset-y = "2%";
+    #     background = "${color.background}";
+    #     foreground = "${color.foreground}";
+    #     radius-top = "0.0";
+    #     radius-bottom = "0.0";
+    #     underline-size = "2";
+    #     underline-color = "${color.foreground}";
+    #     border-size = "0";
+    #     border-color = "${color.background}";
+    #     padding = "0";
+    #     module-margin-left = "0";
+    #     module-margin-right = "0";
+    #     font-0 = "Iosevka Nerd Font:pixelsize=14;4";
+    #     font-1 = "Iosevka Nerd Font:pixelsize=14;4";
+    #     modules-left = "launcher workspaces google github reddit gmail twitter";
+    #     modules-center = "";
+    #     modules-right = "mpd updates alsa battery network date sysmenu";
+    #     separator = "";
+    #     dim-value = "1.0";
+    #     wm-name = "";
+    #     locale = "";
+    #     tray-position = "none";
+    #     tray-detached = "false";
+    #     tray-maxsize = "16";
+    #     tray-background = "${color.background}";
+    #     tray-offset-x = "0";
+    #     tray-offset-y = "0";
+    #     tray-padding = "0";
+    #     tray-scale = "1.0";
+    #     enable-ipc = "true";
+    #     click-left = "";
+    #     click-middle = "";
+    #     click-right = "";
+    #     scroll-up = "";
+    #     scroll-down = "";
+    #     double-click-left = "";
+    #     double-click-middle = "";
+    #     double-click-right = "";
+    #     cursor-click = "";
+    #     cursor-scroll = "";
+    #   };
+    #   "settings" = {
+    #     throttle-output = "5";
+    #     throttle-output-for = "10";
+    #     screenchange-reload = "false";
+    #     compositing-background = "source";
+    #     compositing-foreground = "over";
+    #     compositing-overline = "over";
+    #     compositing-underline = "over";
+    #     compositing-border = "over";
+    #     pseudo-transparency = "false";
+    #
+    #   };
+    # };
     extraConfig = bars + colors + mods + user_mods;
     script = ''exec polybar main'';
   };
